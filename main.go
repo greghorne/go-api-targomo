6a3b2b03e3f8d9bc863dec7ef38567bbf09ba620
Greg Horne <greghorne@hotmail.com>
2018-10-31 20:12:36 +0000

    code updated

--- main.go
+++ main.go
@@ -7,28 +7,18 @@ import (
 	// _ "github.com/lib/pq"
 	"os"
 	// "database/sql"
+	// "io/ioutil"
 	"encoding/json"
-	// "fmt"
+	"strconv"
+	"fmt"
 )
-
-// ============================================================
-// connet string to pg server
-var const_connect_string =" host=" + os.Getenv("GO_HOST") + 
-	" database=" + os.Getenv("GO_DATABASE") + 
-	" user=" + os.Getenv("GO_USER") + 
-	" password=" + os.Getenv("GO_PASSWORD") + 
-	" port=" + os.Getenv("GO_PORT") + 
-	" sslmode=require"
-
-var const_db_type = "postgres"
-// ============================================================
-
 
 // ============================================================
 func main() {
 	
+	fmt.Println("main...")
 	router := mux.NewRouter()
-	router.HandleFunc("/v1/targomo-isochrone/{lng}/{lat}/{time}", v1TargomoIsochrone).Methods("GET")
+	router.HandleFunc("/v1/targomo-isochrone/{lng}/{lat}/{time}/{key}", v1TargomoIsochrone).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8000", router))
 
 }
@@ -40,70 +30,95 @@ func v1TargomoIsochrone (w http.ResponseWriter, r *http.Request) {
 
 	params := mux.Vars(r)
 
-	bIntersects := v1DoTargomoIsochrone(params["lng"], params["lat"], params["time"], params["key"])
-	// jsonResult  := map[string]bool{"intersects": bIntersects}
-
-	json.NewEncoder(w).Encode("x")
-
-}
-// ============================================================
-
-
-func getRegion(xLng string, yLat string) (region string) {
-
-	lng := strconv.ParseFloat(xLng, 64)
-	lat := strconv.ParseFloat(yLat, 64)
-
-	if xLng <= -100 && lat <= 36 {
-		region := "southwest"
-	} else if lng <= -100 && lat >= 36 {
-		region := "northwest"
-	} else if lng >= -100 && lat >= 36 {
-		region := "northeast"
-	} else if lng >= -100 && lat <= 36 {
-		region := "southeast"
+	if isochrone, msg := v1DoTargomoIsochrone(params["lng"], params["lat"], params["time"], params["key"]); msg == "" {
+		//  success
+		fmt.Println(isochrone)
+		fmt.Println("msg: " + msg)
 	} else {
-		region := "none"
+		//error
+		fmt.Println(isochrone)
+		fmt.Println("msg: " + msg)
 	}
 
+	// jsonResult  := map[string]bool{"intersects": bIntersects}
+
+	json.NewEncoder(w).Encode("name:greg")
+
+}
+// ============================================================
+
+
+func getRegion(xLng string, yLat string) (region string, msg string) {
+
+	lng, err := strconv.ParseFloat(xLng, 64)
+	if err != nil {
+		region = "none" 
+		msg = "invalid lng"
+		return
+	}
+	lat, err := strconv.ParseFloat(yLat, 64)
+	if err != nil {
+		region = "none" 
+		msg = "invalid lat"
+		return
+	}
+
+	msg = ""
+
+	if lng <= -100 && lat <= 36 {
+		region = "southwest"
+	} else if lng <= -100 && lat >= 36 {
+		region = "northwest"
+	} else if lng >= -100 && lat >= 36 {
+		region = "northeast"
+	} else if lng >= -100 && lat <= 36 {
+		region = "southeast"
+	} else {
+		msg = "unable to determine region"
+	}
+
+	return
+
 }
 
 
 // ============================================================
-func v1DoTargomoIsochrone(sxLng string, syLat string, sTime string, sKey string) (polygons string) {
+func v1DoTargomoIsochrone(sxLng string, syLat string, sTime string, sKey string) (polygons string, msg string) {
 
-	if region := getRegion(sxLng, syLat); region != "none" { 
+	fmt.Println("v1DoTargomoIsochrone...")
 
-		r360_key := "GYSWOYA0HD8JM1LAJYDX"
+	if region, error_msg := getRegion(sxLng, syLat); error_msg == "" { 
 
-    	r360_url_string := "https://service.route360.net/na_" +
+		r360_key := os.Getenv("TARGOMO")
+
+    	r360_url := "https://service.route360.net/na_" +
 			region + "/v1/polygon?cfg={'sources':[{'lat':" + 
-			xyLat + ",'lng':" + sxLng + 
+			syLat + ",'lng':" + sxLng + 
 			",'id':'Mappy','tm':{'car':{}}}],'polygon':" +
 			"{'serializer':'geojson','srid':'4326'," +
 			"'values':[" + sTime + "]}}&key=" + r360_key
-		polygons = "good"
+		fmt.Println(r360_url)
+
+		response, err := http.Get(r360_url)
+		if err == nil {
+			defer response.Body.Close()
+			fmt.Println(json.NewDecoder(response.Body))
+			// polygons = string(jsonReturned)
+			// msg = ""
+		} else {
+			fmt.Println(err)
+			polygons = ""
+			msg = "some error"
+		}
+
+		// fmt.Println(polygons, msg)
+	
 	} else {
-		polgons = "none"
+		polygons = ""
+		msg = error_msg
 	}
 				  
-	// // connect to db
-	// db, err := sql.Open(const_db_type, const_connect_string)
-	
-	// defer db.Close()
-	// if err != nil { log.Fatal(err) }
-	
-	// // call pg function select z_tl_2016_us_state(lng, lat)
-	// var strQuery = "select z_tl_2016_us_state($1, $2);"
-	// rows, err   := db.Query(strQuery, xLng, yLat)
-	
-	// defer rows.Close()
-	// if err != nil { log.Fatal(err) }
-
-	// bIntersects = false
-	// for rows.Next() {
-	// 	bIntersects = true
-	// } 
+	// fmt.Println(msg)
 
 	return
 
