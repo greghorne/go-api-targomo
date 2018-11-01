d1b5ea2f3875c8c7e40e55587991b8db20d43f60
Greg Horne <greghorne@hotmail.com>
2018-11-01 19:53:35 +0000

    changed port for deployment

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	
 	router := mux.NewRouter()
 	router.HandleFunc("/v1/targomo-isochrone/{lng}/{lat}/{time}/{key}", v1TargomoIsochrone).Methods("GET")
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":8001", router))
 }
 // ============================================================
 
