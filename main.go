30bb65dd58c970b7cf36ef2fe8a64229052886fc
Greg Horne <greghorne@hotmail.com>
2018-12-21 12:03:26 -0600

    cleanup; removed comments

--- main.go
+++ main.go
@@ -83,7 +83,6 @@ func v1DoTargomoIsochrone(sxLng string, syLat string, sTime string, sKey string)
 
 	if region, error_msg := getRegion(sxLng, syLat); error_msg == "" { 
 	
-		// r360_key := os.Getenv("TARGOMO")
     	r360_url := "https://service.route360.net/na_" +
 			region + "/v1/polygon?cfg={'sources':[{'lat':" + 
 			syLat + ",'lng':" + sxLng + 
